631ed82c63122ecbfa3de4f7b264377020ef55fe
Todd <mopeybear@gmail.com>
2018-10-21 10:54:46 -0400

    Buttons for Content Work

--- css/js/script.js
+++
@@ -1,8 +0,0 @@
-//for reference: parameters of project
-//image carousel (rotate min 3 images automatically on page)
-
-//add hamburger button
-
-//add button to show/hide material for portfolio projects
-
-//create modal box to pop up with bio for button or link (prefer for pics for projects as a Zoom function)
