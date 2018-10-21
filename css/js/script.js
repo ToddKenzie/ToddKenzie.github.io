62f64497a8a92cc39a8ffce2f4c5b69715163d74
Todd <mopeybear@gmail.com>
2018-10-21 09:21:45 -0400

    notes for project in JS file

---
+++ css/js/script.js
@@ -0,0 +1,8 @@
+//for reference: parameters of project
+//image carousel (rotate min 3 images automatically on page)
+
+//add hamburger button
+
+//add button to show/hide material for portfolio projects
+
+//create modal box to pop up with bio for button or link (prefer for pics for projects as a Zoom function)
