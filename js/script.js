b86a6b0281d0e546f89c210f1f41247c25401cd8
Todd <mopeybear@gmail.com>
2018-12-12 11:29:19 -0500

    attempting modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var projectButtons = document.querySelectorAll('.show-project');
-var projectInfo = document.querySelectorAll('.project-info');
+const projectButtons = document.querySelectorAll('.show-project');
+const projectInfo = document.querySelectorAll('.project-info');
 
 for (let i = 0; i < projectButtons.length; i++) {
     projectButtons[i].addEventListener('click', function () {
@@ -7,3 +7,34 @@ for (let i = 0; i < projectButtons.length; i++) {
         projectButtons[i].innerText = (projectButtons[i].dataset.toggled ^= 1) ? "Hide Details" : "Show Details";
     })   
 }
+
+// var modal = document.getElementById('modal-contact');
+// var modButton = document.getElementById('modal-button');
+// var modSpan = document.getElementsByClassName('modal-close')[0];
+
+const projectImages = document.getElementsByClassName('.project-image');
+const modalImages = document.getElementsByClassName('.modal-image');
+const modal = document.querySelector('#modal');
+let imageNumber = 0;
+
+for (let j = 0; j < projectImages.length; j++) {
+    projectImages[j].addEventListener('click', function () {
+        modal.style.display = 'block';
+        modalImages[j].style.display = "block";
+        imageNumber = j;
+    })
+}
+
+// modal.addEventListener('click', function () {
+//     modal.style.display = "none";
+// })
+
+window.onclick = event => {
+    if(event.target == modal) {
+        modal.style.display = 'none';
+        modalImages[imageNumber].style.display = "none";
+    }
+}
+
+// modButton.onclick = () => modal.style.display = 'block';
+// modSpan.onclick = () => modal.style.display = 'none';
